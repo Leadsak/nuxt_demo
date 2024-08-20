5e83033d5ba325d83fdfe48e1c2b526f4d649380
Leadsak <zam210641@gmail.com>
2024-08-20 21:01:45 +0700

    upanddown

--- app.vue
+++ app.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class=" bg-[#6a727f] w-auto h-[30px]"> 
+    </div>
+
+  <div class="flex  justify-center">
+    <div class="bg-[#85eeab] w-[60%] h-screen grid grid-cols-2 p-8  ">
+        <div class="flex justify-center items-center h-[200px]  ">
+          <div class=" border-[1px] border-black  rounded-full  w-[100px] h-[100px] " ></div>
+      </div>
+      <div class="grid grid-rows-subgrid gap-4 row-span-3 " >
+      <div class="flex flex-col mt-10 gap-5 ">
+        <div class=" border-[1px] border-black  rounded  w-[434px] h-[30px]   " ></div>
+        <div class=" border-[1px] border-black  rounded  w-[434px] h-[30px]   " ></div>   
+        <div class="row-start-3 " >
+              <div class="flex justify-end ">
+              <div class="rounded-full w-[50px] text-center  bg-[#0ea2e4] text-gray-50 ">save</div>
+              </div>
+            </div>
+        
+        </div>
+             
+            <div class="row-start-3  " >
+              <div class="flex justify-end">
+              <div class=" rounded-full w-[50px] text-center  bg-[#0ea2e4] text-gray-50">save</div>
+              </div>
+            </div>
+      </div>
+    </div>
   </div>
 </template>
+
+
